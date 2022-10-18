8908293724c1ded01c1d7a3dff4cc5975d2f198e
divyansh-tamrakar <divyansh.tamrakar17@gmail.com>
2022-10-19 02:08:41 +0530

    frontend updated

--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from bs4 import BeautifulSoup
+import requests
 # Create your views here.
 
 
 def home(request):
     return render(request, 'base.html')
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {
+        'search': search
+    }
+
+    return render(request, 'app/new_search.html', context)
